20ac34fdf797e3bcfaacf69ca7af085d8e368cee
test01 <test01@naver.com>
2023-04-25 21:15:11 +0900

    수정

--- src/quiz/MainQ4.java
+++ src/quiz/MainQ4.java
@@ -4,21 +4,14 @@ import java.util.Scanner;
 
 public class MainQ4 {
 public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	int[] num = new int[2];
 	
-	for(int i=0;i<num.length;i++) {
-		System.out.print("수 입력: ");
-		num[i] = input.nextInt();
-	}
+	TestQ4 t = new TestQ4();
+	int n = t.myinput();
+	int c = t.prime(n);
+	t.print(c);
 	
-//	for(int i=0;i<num.length;i++) {
-//		if(num[i]==) {
-//			System.out.println(num[i]+" (소수)");
-//		}else {
-//			System.out.println(num[i]+" (소수아님)");
-//		}
-//	}
+
+
 	
 
 }
--- src/quiz/TestQ4.java
+++ src/quiz/TestQ4.java
@@ -1,5 +1,36 @@
 package quiz;
 
-public class TestQ4 {
+import java.util.Scanner;
 
+public class TestQ4 {
+	public int myinput() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("수 입력");
+		int num = input.nextInt();
+		
+		return num;
+	}
+	
+	public int prime(int num) {
+		int cnt=0;
+		for(int i = 1;i<=num;i++) {
+			if(num%i==0) {
+				//System.out.println(i+"약수");
+				cnt++;
+			}else {
+				//System.out.println(i+"약수 아님");
+			}
+		
+		}
+		
+		return cnt;
+	}
+	
+	public void print(int cnt) {
+		if(cnt==2) {
+			System.out.println("소수입니다");
+		}else {
+			System.out.println("소수 아님");
+		}
+	}
 }
